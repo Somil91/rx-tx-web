77b0cac80846f36ea0278ba03bf020696033cf85
SomilKumar <somil.kumar91@gmail.com>
2019-01-16 18:18:56 +0100

    Changes for the stateful typed component

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,36 @@ type AppProps = {message : string}
 
 
 // const App = ({message}) =>   <div>Hello World</div>;
-const App :React.SFC<AppProps> = ({message}) =>   <div>{message}</div>;
+/** stateless component */
+// const App :React.SFC<AppProps> = ({message}) =>   <div>{message}</div>;
+
+/**
+ * stateful components
+ */
+
+class App extends React.Component<{
+    message : string
+    }, {
+        count : number;
+    }> {
+        constructor(props) {
+            super(props);
+            this.state = {
+                count: 0
+            }
+        }
+        render () {
+            return (
+                <div onClick={this.increment}>{this.props.message} {this.state.count}</div>
+            )
+    }
+
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+}
 
 ReactDOM.render(
      <App message="hello world with props"/>,
