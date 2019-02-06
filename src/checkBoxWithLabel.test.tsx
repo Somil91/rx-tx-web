b628b6e7830a1c192037299f042289a0785baa7f
SomilKumar <somil.kumar91@gmail.com>
2019-02-06 17:50:12 +0100

    Dummy commit

--- src/checkBoxWithLabel.test.tsx
+++ src/checkBoxWithLabel.test.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import { CheckBoxWithLabel } from './checkBoxWithLabel';
 
-
 test('checkboxwithlabel changes the text after click', () => {
     const checkbox = shallow(<CheckBoxWithLabel labelOn="On" labelOff="Off"/>);
     expect(checkbox.text()).toEqual('Off');
